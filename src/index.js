fd007ec5e4a5f423acbba4b91ccd4079f10ddfd1
pablo128482 <ashley.c.clay@gmail.com>
2023-07-27 14:16:50 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch(imgUrl)
+  .then(response => response.json())
+  .then(data => {
+    // response added with data
+  })
+  .catch(error => {
+    //the error is resolved
+  });
